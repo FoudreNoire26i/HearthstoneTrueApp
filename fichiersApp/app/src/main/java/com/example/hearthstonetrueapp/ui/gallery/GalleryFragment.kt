d12912f1fcf0aa70eec718bdcf9cd6110266a9e9
Benjamin <@p1807300>
2020-11-03 14:38:06 +0100

    remove deprecated func

--- fichiersApp/app/src/main/java/com/example/hearthstonetrueapp/ui/gallery/GalleryFragment.kt
+++ fichiersApp/app/src/main/java/com/example/hearthstonetrueapp/ui/gallery/GalleryFragment.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
 import com.example.hearthstonetrueapp.R
 
@@ -20,7 +21,7 @@ class GalleryFragment : Fragment() {
             savedInstanceState: Bundle?
     ): View? {
         galleryViewModel =
-                ViewModelProviders.of(this).get(GalleryViewModel::class.java)
+                ViewModelProvider(this).get(GalleryViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_gallery, container, false)
         val textView: TextView = root.findViewById(R.id.text_gallery)
         galleryViewModel.text.observe(viewLifecycleOwner, Observer {
--- fichiersApp/app/src/main/java/com/example/hearthstonetrueapp/ui/slideshow/SlideshowFragment.kt
+++ fichiersApp/app/src/main/java/com/example/hearthstonetrueapp/ui/slideshow/SlideshowFragment.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
 import com.example.hearthstonetrueapp.R
 
@@ -20,7 +21,7 @@ class SlideshowFragment : Fragment() {
             savedInstanceState: Bundle?
     ): View? {
         slideshowViewModel =
-                ViewModelProviders.of(this).get(SlideshowViewModel::class.java)
+                ViewModelProvider(this).get(SlideshowViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_slideshow, container, false)
         val textView: TextView = root.findViewById(R.id.text_slideshow)
         slideshowViewModel.text.observe(viewLifecycleOwner, Observer {
