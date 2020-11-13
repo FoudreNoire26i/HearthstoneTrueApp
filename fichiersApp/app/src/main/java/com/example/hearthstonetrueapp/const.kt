79819eb7f06b5b1028cc61c41799bbdf5f3522b2
Benjamin <@p1807300>
2020-11-13 14:14:25 +0100

    const et splash

--- fichiersApp/app/src/main/java/com/example/hearthstonetrueapp/const.kt
+++ fichiersApp/app/src/main/java/com/example/hearthstonetrueapp/const.kt
@@ -1,6 +1,9 @@
 package com.example.hearthstonetrueapp
 
-const val BASE_URL = "https://us.api.blizzard.com/hearthstone/"
-const val accessToken = "USRcxfthFjLJKNmRyVMTv5sLd78TIaIIqj"
+const val BASE_URL_HS = "https://eu.api.blizzard.com/hearthstone/"
+const val BASE_URL_TOKEN = "https://eu.battle.net/oauth/"
+const val client_id = "29485ac829354019b30635cd74b43807"
+const val client_secret = "ZnObmgWOpyF1DmjnmI1WC7GBbQPZN1gI"
+var accessToken = ""
 const val tokenType = "Bearer"
 const val local = "fr_FR"
